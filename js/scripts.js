f31518c8dd39214a2758bf563d6549e24ecd75e2
Matheus Colombo <111023158+mathcolombo@users.noreply.github.com>
2024-06-30 15:35:23 -0300

    Finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
-/*!
-* Start Bootstrap - Coming Soon v6.0.7 (https://startbootstrap.com/theme/coming-soon)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-coming-soon/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+function copiar() {
+    navigator.clipboard.writeText('https://o-apito.vercel.app');
+
+    alert('URL do site em produção copiado com sucesso!!')
+}
